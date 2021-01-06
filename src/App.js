51f7e3f08e5b74a75e59c400f321115890947bb7
Syed Aashir Majeed <syedaashir120@gmail.com>
2021-01-06 15:43:49 +0500

    chnages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { AccountSummary } from './components/AccountSummary';
+import { TransactionList } from './components/TransactionHistory';
+import { AddTransaction } from './components/AddTransaction';
+import firebase from './firebase';
+
+import { GlobalProvider } from './context/GlobalState';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const messaging = firebase.messaging();
+    messaging.requestPermission().then(() => {
+        return messaging.getToken();
+    }).then((token) => {
+        console.log("token", token)
+
+    })
+
+    return ( <
+        GlobalProvider >
+        <
+        Header / >
+        <
+        div className = "container" >
+        <
+        Balance / >
+        <
+        AccountSummary / >
+        <
+        TransactionList / >
+        <
+        AddTransaction / >
+        <
+        /div> <
+        / GlobalProvider >
+    );
 }
 
 export default App;
